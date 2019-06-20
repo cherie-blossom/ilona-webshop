29554c184f6be20afde6f12712ccc7368f01e971
cherieBlossom <cheriesofteng@gmail.com>
2019-06-20 10:37:39 +0200

    Create footer in custom WordPress theme

---
+++ footer.php
@@ -0,0 +1,10 @@
+<footer class="footer"> &reg;&nbsp;2019 Ilona Rubstova | 
+  <a href="mailto:ilonarubtsova@gmail.com">ilonarubtsova@gmail.com</a>
+  <br>
+  Developed with love by&nbsp;
+  <a href="#" class="brand-name" >Cherie Blossom</a>
+</footer>
+
+<?php wp_footer(); ?>
+</body>
+</html>
